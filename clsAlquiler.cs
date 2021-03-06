c570973676142bc9ca65cfcb174af6f4e6cb43a1
LouMDVP11 <66640684+LouMDVP11@users.noreply.github.com>
2021-03-06 12:00:51 -0600

    Agregar archivos de proyecto.

---
+++ clsAlquiler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerDeAutos
+{
+    class clsAlquiler
+    {
+        int nit, kmRecorridos;
+        string placa;
+        DateTime fechaAlquiler, fechaDevolucion;
+
+        public int Nit { get => nit; set => nit = value; }
+        public int KmRecorridos { get => kmRecorridos; set => kmRecorridos = value; }
+        public string Placa { get => placa; set => placa = value; }
+        public DateTime FechaAlquiler { get => fechaAlquiler; set => fechaAlquiler = value; }
+        public DateTime FechaDevolucion { get => fechaDevolucion; set => fechaDevolucion = value; }
+    }
+}
---
+++ clsVehiculo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerDeAutos
+{
+    public class clsVehiculo
+    {
+        string placa, marca, modelo, color;
+        double precioPorKm;
+
+        public string Placa { get => placa; set => placa = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public string Color { get => color; set => color = value; }
+        public double PrecioPorKm { get => precioPorKm; set => precioPorKm = value; }
+    }
+}
